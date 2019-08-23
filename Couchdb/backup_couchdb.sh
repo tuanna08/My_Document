a6777bb09ec80bfe9545b624c060877f7995ac3f
tuanna08 <52945640+tuanna08@users.noreply.github.com>
2019-08-23 11:04:04 +0700

    Update backup_couchdb.sh

--- Couchdb/backup_couchdb.sh
+++ Couchdb/backup_couchdb.sh
@@ -3,17 +3,17 @@
 DATE_STR=$(date '+%d-%b-%Y')
 DATE_TIME_STR=$(date +"%d-%b-%YT%T")
 
-URL="http://admin:123456abcA@10.10.2.212:5984"
+URL="http://your_account:your_pass_couchdb@your_id:5984"
 # backup database
 # db name
-DB_NAME[0]="heovang_blockchain_db"
-DB_NAME[1]="heovang_merchant_cms"
-DB_NAME[2]="heovang_notify_db"
-DB_NAME[3]="heovang_pda_db"
-DB_NAME[4]="heovang_private_security_db"
-DB_NAME[5]="heovang_saltstore_db"
-DB_NAME[6]="heovang_transaction_log"
-DB_NAME[7]="heovang_userstores_db"
+DB_NAME[0]="dbname1"
+DB_NAME[1]="db_name2"
+DB_NAME[2]="db_name2"
+DB_NAME[3]="db_name2"
+DB_NAME[4]="db_name2"
+DB_NAME[5]="db_name2"
+DB_NAME[6]="db_name2"
+DB_NAME[7]="db_name2"
 
 
 #DB_NAME_LARGE[0]="heovang_merchant_cms"
