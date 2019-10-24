ef53aaa344ffe7b78c5f3d18592d4588f21121e3
tuanna08 <52945640+tuanna08@users.noreply.github.com>
2019-10-24 09:00:12 +0700

    Update backup_mysql.sh

--- backup_mysql/backup_mysql.sh
+++ backup_mysql/backup_mysql.sh
@@ -3,7 +3,6 @@ echo "<=========================== Program by Nguyễn Anh Tuấn===============
 DATE_STR=$(date '+%d-%b-%Y')
 DATE_TIME_STR=$(date +"%d-%b-%YT%T")
 
-URL="http://your_account:your_pass_couchdb@your_id:5984"
 # backup database
 # db name
 DB_NAME[0]="cinder"
@@ -15,9 +14,6 @@ DB_NAME[5]="nova_api"
 DB_NAME[6]="nova_cell0"
 DB_NAME[7]="nova_placement"
 
-
-#DB_NAME_LARGE[0]="heovang_merchant_cms"
-
 for i in "${DB_NAME[@]}"
 do	
 	mkdir -p $HOME/backup_mysql/$DATE_STR/
@@ -25,4 +21,4 @@ do
 	
 done
 
-echo "Backup dữ liệu heo vang thành công: "$DATE_TIME_STR >> $HOME/backup_mysql/log_backup.out
+echo "Backup dữ liệu openstack thành công: "$DATE_TIME_STR >> $HOME/backup_mysql/log_backup.out
