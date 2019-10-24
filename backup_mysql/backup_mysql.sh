660751a75b2016669a7cba06c9797b3c8311b133
tuanna08 <52945640+tuanna08@users.noreply.github.com>
2019-10-24 08:59:06 +0700

    Update backup_mysql.sh

--- backup_mysql/backup_mysql.sh
+++ backup_mysql/backup_mysql.sh
@@ -6,22 +6,23 @@ DATE_TIME_STR=$(date +"%d-%b-%YT%T")
 URL="http://your_account:your_pass_couchdb@your_id:5984"
 # backup database
 # db name
-DB_NAME[0]="dbname1"
-DB_NAME[1]="db_name2"
-DB_NAME[2]="db_name2"
-DB_NAME[3]="db_name2"
-DB_NAME[4]="db_name2"
-DB_NAME[5]="db_name2"
-DB_NAME[6]="db_name2"
-DB_NAME[7]="db_name2"
+DB_NAME[0]="cinder"
+DB_NAME[1]="glance"
+DB_NAME[2]="keystone"
+DB_NAME[3]="neutron_ml2"
+DB_NAME[4]="nova"
+DB_NAME[5]="nova_api"
+DB_NAME[6]="nova_cell0"
+DB_NAME[7]="nova_placement"
 
 
 #DB_NAME_LARGE[0]="heovang_merchant_cms"
 
 for i in "${DB_NAME[@]}"
-do
-	mkdir -p $HOME/backup_couchdb/$DATE_STR/
-	couchbackup "--url" $URL "--db" $i > $HOME"/backup_couchdb/"$DATE_STR"/"$i".json"
+do	
+	mkdir -p $HOME/backup_mysql/$DATE_STR/
+	mysqldump --databases $i > $HOME"/backup_mysql/"$DATE_STR"/"$i".sql"
+	
 done
 
-echo "Backup dữ liệu heo vang thành công: "$DATE_TIME_STR >> $HOME/backup_couchdb/log_backup.out
+echo "Backup dữ liệu heo vang thành công: "$DATE_TIME_STR >> $HOME/backup_mysql/log_backup.out
